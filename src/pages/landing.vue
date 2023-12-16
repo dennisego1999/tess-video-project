fb0808318f80f199c433749ed1183ead49f805f9
Dennis Ego <dennis.ego@artcoresociety.com>
2023-12-16 11:11:52 +0100

    add video rows dynamically

--- src/pages/landing.vue
+++ src/pages/landing.vue
@@ -5,12 +5,23 @@ import { nextTick, onBeforeUnmount, ref } from 'vue';
 let circleDimensions = null;
 const circle = ref(null);
 
+const videosData = [
+	{ videos: ['F1.mp4', 'F2.mp4', 'F3.mp4', 'F4.mp4', 'F5.mp4', 'F6.mp4'] },
+	{ videos: ['F1.mp4', 'F2.mp4'] },
+	{ videos: ['F1.mp4'] },
+	{ videos: ['F1.mp4'] },
+	{ videos: ['F1.mp4'] },
+	{ videos: ['F1.mp4'] }
+];
+
 // Define functions
 function onMouseMove(event) {
 	// Set the position of the circle
 	circle.value.style.top = event.offsetY - circleDimensions.height / 2 + 'px';
 	circle.value.style.left = event.offsetX - circleDimensions.width / 2 + 'px';
 	circle.value.style.transform = 'none';
+
+	console.log('moving');
 }
 
 function getCircleDimensions() {
@@ -40,5 +51,25 @@ onBeforeUnmount(() => {
 <template>
 	<div class="relative h-screen w-screen pointer-events-none">
 		<div ref="circle" id="drag-circle"></div>
+
+		<div class="relative z-0 flex flex-col justify-start items-start]">
+			<div
+				v-for="(row, index) in videosData"
+				:key="'video-row-' + (index + 1)"
+				class="flex justify-start items-center h-full w-fit"
+			>
+				<video
+					v-for="(video, videosIndex) in row.videos"
+					:key="'video' + (videosIndex + 1)"
+					class="h-screen w-screen object-cover"
+					muted
+					autoplay
+					loop
+					playsinline
+				>
+					<source :src="'/assets/videos/' + (index + 1) + '/' + video" />
+				</video>
+			</div>
+		</div>
 	</div>
 </template>
