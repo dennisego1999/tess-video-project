851e869e9fa9f7f0bad6d35ce6488cb47d54491a
Dennis Ego <dennis.ego@artcoresociety.com>
2024-01-07 15:43:19 +0100

    add animated title to landing

--- src/pages/landing.vue
+++ src/pages/landing.vue
@@ -1,10 +1,16 @@
 <script setup>
-import { nextTick } from 'vue';
+import { nextTick, ref } from 'vue';
+import { gsap } from 'gsap';
 
 // Define emits
 const emit = defineEmits(['set-is-hovering']);
 
 // Set variables
+const isAnimatedTitleActive = ref(true);
+const animatedTitleRows = ref([
+	{ id: 'first_row', chars: ['t', 'e', 's', 's'] },
+	{ id: 'second_row', chars: ['e', 'g', 'o'] }
+]);
 const videoRows = [
 	{ videos: ['F1.mp4', 'F2.mp4', 'F3.mp4', 'F4.mp4', 'F5.mp4', 'F6.mp4'] },
 	{ videos: ['F1.mp4', 'F2.mp4'] },
@@ -37,11 +43,42 @@ nextTick(() => {
 	// Check scrollable section
 	const firstVideoRow = document.getElementById('video-row-1');
 	checkIfHoveringScrollableRow(firstVideoRow.children);
+
+	setTimeout(() => {
+		// Animate out
+		gsap.to('.animated-title-char', {
+			scale: 0,
+			rotation: 20,
+			yPercent: 50,
+			duration: 0.4,
+			stagger: -0.1,
+			ease: 'power1.inOut',
+			onComplete: () => {
+				// Update reactive
+				isAnimatedTitleActive.value = false;
+			}
+		});
+	}, 2000);
 });
 </script>
 
 <template>
 	<div class="relative pointer-events-none scrollbar-hide overflow-hidden">
+		<div
+			v-if="isAnimatedTitleActive"
+			class="fixed z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center"
+		>
+			<div v-for="row in animatedTitleRows" :key="row.id" class="flex justify-center items-center gap-[50px]">
+				<p
+					v-for="(char, index) in row.chars"
+					:key="'char-' + index"
+					class="animated-title-char font-arialRounded font-outline-7 stroke-primary-red text-primary-yellow uppercase text-[300px] leading-[normal]"
+				>
+					{{ char }}
+				</p>
+			</div>
+		</div>
+
 		<div class="relative z-0 flex flex-col justify-start items-start pointer-events-none">
 			<div
 				v-for="(row, index) in videoRows"
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				arialRounded: ['Arial Rounded MT Bold']
+			},
 			colors: {
 				primary: {
 					red: '#FF0000',
